1f9ae89d9836c849727a88c5c7451510eec6a153
jzl1 <jzl1@asb9838nu-b07.csil.sfu.ca>
2017-09-13 18:41:05 -0700

    t2 submission

---
+++ 1/t2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+   printf("%lu %lu %lu %lu %lu %lu %lu %lu %lu\n",sizeof(char),sizeof(unsigned char),sizeof(int),sizeof(unsigned int),sizeof(long),sizeof(unsigned long),sizeof(float),sizeof(double),sizeof(long double));
+   return 0;
+}
