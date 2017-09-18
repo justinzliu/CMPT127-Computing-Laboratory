82fcfea761346d391522b79afd12b36fcf7b79aa
jzl1 <jzl1@asb9804u-c05.csil.sfu.ca>
2017-09-18 13:05:45 -0700

    t2 submission (edit)

--- 1/t2.c
+++ 1/t2.c
@@ -2,6 +2,15 @@
 
 int main(void)
 {
-   printf("%lu %lu %lu %lu %lu %lu %lu %lu %lu\n",sizeof(char),sizeof(unsigned char),sizeof(int),sizeof(unsigned int),sizeof(long),sizeof(unsigned long),sizeof(float),sizeof(double),sizeof(long double));
+   unsigned long schar=sizeof(char)*8;
+   unsigned long suchar=sizeof(unsigned char)*8;
+   unsigned long sint=sizeof(int)*8;
+   unsigned long suint=sizeof(unsigned int)*8;
+   unsigned long slong=sizeof(long)*8;
+   unsigned long sulong=sizeof(unsigned long)*8;
+   unsigned long sfloat=sizeof(float)*8;
+   unsigned long sdouble=sizeof(double)*8;
+   unsigned long sldouble=sizeof(long double)*8;
+   printf("%lu %lu %lu %lu %lu %lu %lu %lu %lu\n", schar, suchar, sint, suint, slong, sulong, sfloat, sdouble, sldouble);
    return 0;
 }
