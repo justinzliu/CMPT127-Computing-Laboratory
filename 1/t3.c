af5c8c9f1ae86c0b094f6b510a956613ec1bfab9
jzl1 <jzl1@asb9804u-c05.csil.sfu.ca>
2017-09-18 14:20:56 -0700

    t3 submission

---
+++ 1/t3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+   float i = 0.0;
+   printf("Enter an floating point value: ");
+   scanf("%f", &i);
+   float rounddown = floor(i);
+   float stdround = round(i);
+   float roundup = ceil(i);
+   printf("%0.0f %0.0f %0.0f\n", rounddown, stdround, roundup);
+   return 0;
+}
