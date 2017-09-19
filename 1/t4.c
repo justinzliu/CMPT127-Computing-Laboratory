446ed5866e0d1637c6c83c8b6f8e98f8aa7743a0
jzl1 <jzl1@asb9804-a08.csil.sfu.ca>
2017-09-19 15:44:24 -0700

    t4 submission

---
+++ 1/t4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+   float i;
+   int check;
+   printf("Enter an floating point value: ");
+   check = scanf("%f", &i);
+   if (check != 1)
+   {
+     printf("scanf error: (%d)\n", check);
+   }
+   else
+   {
+     float rounddown = floor(i);
+     float stdround = round(i);
+     float roundup = ceil(i);
+     printf("%0.0f %0.0f %0.0f\n", rounddown, stdround, roundup);
+   }
+   return 0;
+}
