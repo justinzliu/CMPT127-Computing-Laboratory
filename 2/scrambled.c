da2dc65293de6fa2193a1f6a9619ae535b7ba2a5
jzl1 <jzl1@asb9804u-d04.csil.sfu.ca>
2017-10-08 18:37:08 -0700

    scrambled is working

---
+++ 2/scrambled.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int scrambled(unsigned int a[], unsigned int b[], unsigned int len){
+   int testsuma = 0;
+   int testsumb = 0;
+   int testmula = 1;
+   int testmulb = 1;
+   for (int i=0; i<len; i++){
+      if (len == 0){
+      i = -2;
+      }
+      else {
+         testsuma += a[i];
+         testsumb += b[i];
+         testmula = a[i] * testmula;
+         testmulb = b[i] * testmulb;
+      }
+   }
+   if (testsuma == testsumb && testmula == testmulb){
+      return 1;
+   }
+   else {
+      return 0;
+   }
+}
