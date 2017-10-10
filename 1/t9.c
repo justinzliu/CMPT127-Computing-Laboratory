7d98544f38cba1442d83286d9fa2fc0d30e780cf
jzl1 <jzl1@asb9838nu-a16.csil.sfu.ca>
2017-10-09 17:57:23 -0700

    t9 done

---
+++ 1/t9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+   int pyramid[3];
+   int check;
+   int xleft;
+   int xright;
+   for(int i=0; i<3; i++) {
+      scanf("%d", &pyramid[i]);
+   }
+   for(int i=pyramid[2]; i>0; i--){
+   int z=+1;
+   xleft = floor( (i-1) * (pyramid[0]/(2.0*pyramid[2])) );
+   xright = ceil( (pyramid[0]-1) + -(i-1) * (pyramid[0]/(2.0*pyramid[2])) ); 
+      if (i==1){
+         for(int j=0; j<=pyramid[0]-1; j++){
+            printf("#");
+         }
+      }
+      else {
+         for(int j=0; j<=pyramid[0]-1; j++){
+            if (j == xleft || j == xright) {
+               printf("#");
+            }
+            else if (j > xleft && j < xright) {
+               if (i == pyramid[2]) {
+                  printf("#");
+               }
+               else {
+                  printf(".");
+               }
+            }
+            else {
+               printf(" ");
+            }
+         }
+      } 
+      printf("\n");   
+   }
+   return 0;
+}
