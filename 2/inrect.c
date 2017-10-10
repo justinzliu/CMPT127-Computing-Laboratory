28c2b51d629175643a68e816c629fb0876ed26af
jzl1 <jzl1@asb9838nu-a16.csil.sfu.ca>
2017-10-09 21:39:14 -0700

    inrect.c done

---
+++ 2/inrect.c
@@ -0,0 +1,39 @@
+int InRectangle( float pt[2], float rect[4] ) {
+   int xmin;
+   int xmax;
+   int ymin;
+   int ymax;
+   if (rect[0] > rect[2]) {
+      xmax = rect[0];
+      xmin = rect[2];
+   }
+   else if (rect[2] > rect[0]) {
+      xmax = rect[2];
+      xmin = rect[0];
+   }
+   else if (rect[0] == rect[2]) {
+      xmax = rect[0];
+      xmin = rect[0];
+   }
+   if (rect[1] > rect[3]) {
+      ymax = rect[1];
+      ymin = rect[3];
+   }
+   else if (rect[2] > rect[0]) {
+      ymax = rect[3];
+      ymin = rect[1];
+   }
+   else if (rect[0] == rect[2]) {
+      ymax = rect[1];
+      ymin = rect[1];
+   }
+   if (pt[0] > xmax || pt[0] < xmin) {
+      return 0;
+   }
+   else if (pt[1] > ymax || pt[1] < ymin) {
+      return 0;
+   }
+   else {
+      return 1;
+   }
+}
