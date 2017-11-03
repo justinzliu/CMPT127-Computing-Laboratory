5b9499451593783a71e1c000967b6067d3e9d6ce
Justin <jzl1@sfu.ca>
2017-11-02 20:50:40 -0700

    Test

--- 4/t1.c
+++ 4/t1.c
@@ -10,16 +10,32 @@ void draw_circle(uint8_t img[],
                  int r,
                  uint8_t color )
 {
+   r = r*r;
+   float distance;
    if (r == 0) {
    }
    else {
       for (int i=0; i<rows; i++) {
          for (int j=0; j<cols; j++) {
-            int distance = round(sqrt((pow((j-x),2)+pow((i-y),2))/1.0));
+            if (j == x || i == y) {                                // PIXEL LIES ON THE X OR Y AXIS
+               distance = pow((j-x),2)+pow((i-y),2);
+            }
+            else if (j > x && i > y) {                                  // PIXEL LIES IN QUADRANT 1
+               distance = pow((j-x+0.5),2)+pow((i-y+0.5),2);
+            }
+            else if (j < x && i > y) {                                  // PIXEL LIES IN QUADRANT 2
+               distance = pow((j-x-0.5),2)+pow((i-y+0.5),2);
+            }
+            else if (j < x && i < y) {                                  // PIXEL LIES IN QUADRANT 3
+               distance = pow((j-x-0.5),2)+pow((i-y-0.5),2);
+            }
+            else if (j > x && i < y) {                                  // PIXEL LIES IN QUADRANT 4
+               distance = pow((j-x+0.5),2)+pow((i-y-0.5),2);
+            }
             if (distance <= r) {
                img[j+i*cols] = color;
             }
-         }
+         }            
       }
    }
    return;
