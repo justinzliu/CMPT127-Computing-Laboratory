65f914a38819aca61c29375a027bca52cd4cb31f
jzl1 <jzl1@asb9838nu-d04.csil.sfu.ca>
2017-11-06 13:50:51 -0800

    lab4 complete

--- 4/t1.c
+++ 4/t1.c
@@ -11,32 +11,24 @@ void draw_circle(uint8_t img[],
                  uint8_t color )
 {
    r = r*r;
-   float distance;
+   float distarray[5];
    if (r == 0) {
    }
    else {
       for (int i=0; i<rows; i++) {
-         for (int j=0; j<cols; j++) {
-            if (j == x || i == y) {                                // PIXEL LIES ON THE X OR Y AXIS
-               distance = pow((j-x),2)+pow((i-y),2);
-            }
-            else if (j > x && i > y) {                                  // PIXEL LIES IN QUADRANT 1
-               distance = pow((j-x+0.5),2)+pow((i-y+0.5),2);
-            }
-            else if (j < x && i > y) {                                  // PIXEL LIES IN QUADRANT 2
-               distance = pow((j-x-0.5),2)+pow((i-y+0.5),2);
-            }
-            else if (j < x && i < y) {                                  // PIXEL LIES IN QUADRANT 3
-               distance = pow((j-x-0.5),2)+pow((i-y-0.5),2);
-            }
-            else if (j > x && i < y) {                                  // PIXEL LIES IN QUADRANT 4
-               distance = pow((j-x+0.5),2)+pow((i-y-0.5),2);
-            }
-            if (distance <= r) {
-               img[j+i*cols] = color;
-            }
-         }            
-      }
+         for (int j=0; j<cols; j++) {                           
+            distarray[0] = pow((j-x),2)+pow((i-y),2);                       
+            distarray[1] = pow((j-x+0.5),2)+pow((i-y+0.5),2);                                
+            distarray[2] = pow((j-x-0.5),2)+pow((i-y+0.5),2);              
+            distarray[3] = pow((j-x-0.5),2)+pow((i-y-0.5),2);                             
+            distarray[4] = pow((j-x+0.5),2)+pow((i-y-0.5),2);
+         for (int k=0; k<5; k++) {
+               if (distarray[k] < r) {
+                  img[j+i*cols] = color;
+               }
+         }
+         }
+      }            
    }
    return;
 }
