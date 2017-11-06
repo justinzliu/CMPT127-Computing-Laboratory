14523d15c31878d4da00b09ee20a3c2dbae93101
Justin <jzl1@sfu.ca>
2017-11-05 22:10:11 -0800

    test

--- 5/intarr.c
+++ 5/intarr.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 typedef struct {
    int* data;
    unsigned int len;
@@ -13,13 +13,15 @@ typedef enum {
   INTARR_BADALLOC,
   INTARR_NOTFOUND
 } intarr_result_t;
+*/
 
 /* LAB 5 TASK 1 */
 
 // Create a new intarr_t with initial size len.  If successful
 // (i.e. memory allocation succeeds), returns a pointer to a
 // newly-allocated intarr_t.  If unsuccessful, returns a null pointer.
-intarr_t* intarr_create( unsigned int len ) {
+intarr_t* intarr_create( unsigned int len ) 
+{
    intarr_t* ptr = malloc(len);
    if (ptr != 0) {
       return ptr;
@@ -29,7 +31,8 @@ intarr_t* intarr_create( unsigned int len ) {
 
 // frees all memory allocated for ia. If the pointer is null, do
 // nothing. If the ia->data is null, do not attempt to free it.
-void intarr_destroy( intarr_t* ia ) {
+void intarr_destroy( intarr_t* ia ) 
+{
    if (ia->data != NULL) {
       free(ia);
    }
@@ -43,7 +46,15 @@ void intarr_destroy( intarr_t* ia ) {
 // INTARR_BADINDEX. If ia is null, return INTARR_BADARRAY.
 intarr_result_t intarr_set( intarr_t* ia, 
 			    unsigned int index, 
-			    int val ) {
+			    int val ) 
+{
+   if (ia == NULL) {
+      return INSTARR_BADINDEX;
+   }
+   else if (index < sizeof(ia->data)/sizeof(int)) {
+      ia->data[index] = val;
+      return INTARR_OK;
+   }
    return INTARR_BADARRAY;
 }
 
@@ -52,8 +63,11 @@ intarr_result_t intarr_set( intarr_t* ia,
 // INTARR_BADINDEX. If ia is null, return INTARR_BADARRAY.
 intarr_result_t intarr_get( const intarr_t* ia, 
 			    unsigned int index, 
-			    int* i ) {
-   return INTARR_BADARRAY;
+			    int* i ) 
+{
+   else if (index < sizeof(ia->data)/sizeof(int)) {
+      ia->data[index] = val;
+      return INTARR_OK;
 }
 
 /* LAB 5 TASK 3 */
