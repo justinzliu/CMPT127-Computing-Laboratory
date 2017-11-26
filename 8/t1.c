467158dcfc8f6246e376caebe52c761677a505c5
Justin <jzl1@sfu.ca>
2017-11-25 21:35:48 -0800

    test

---
+++ 8/t1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "point_array.h"
+
+// Safely initalize an empty array structure.
+void point_array_init( point_array_t* pa ) {
+   if (pa != NULL) {
+      pa->points = malloc(sizeof(point_t)*pa->len);
+   }
+   return;
+}
+
+// Resets the array to be empty, freeing any memory allocated if
+// necessary.
+void point_array_reset( point_array_t* pa ) {
+   if (pa != NULL && pa->points != NULL) {
+      free(pa->points);
+   }
+   return;
+}
+
+// Append a point to the end of an array. If successful, return 0,
+// else return 1;
+int point_array_append( point_array_t* pa, point_t* p ) {
+   if (pa != NULL && pa->points != NULL) {
+      pa->points = realloc(pa->points,sizeof(point_t)*(pa->len+1));
+      if (pa->points != NULL) {
+         pa->points[pa->len] = *p;
+         pa->len = pa->len+1;
+         return 0;
+      }
+   }
+   return 1;
+}
+
+// Remove the point at index i from the array, reducing the size of
+// the array by one. The order of points in the array may change.
+int point_array_remove( point_array_t* pa, unsigned int i ) {
+   if (pa != NULL && pa->points != NULL) {
+      pa->points[i] = pa->points[pa->len-1];
+      pa->points = realloc(pa->points,sizeof(point_t)*(pa->len-1));
+      pa->len = pa->len-1;
+      return 0;
+   }
+   return 1;
+}
+
+
