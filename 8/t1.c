4d4e158192552829c131211a4654a682902405ba
jzl1 <jzl1@asb9838nu-d06.csil.sfu.ca>
2017-11-30 20:31:11 -0800

    wee

--- 8/t1.c
+++ 8/t1.c
@@ -29,11 +29,6 @@ int point_array_append( point_array_t* pa, point_t* p ) {
       pa->points = realloc(pa->points,sizeof(point_t)*(pa->len+1));
       if (pa->points != NULL) {
          pa->points[pa->len] = *p;
-         /*
-         pa->points[pa->len].x = p->x;
-         pa->points[pa->len].y = p->y;
-         pa->points[pa->len].z = p->z;
-         */
          pa->len = pa->len+1;
          return 0;
       }
@@ -46,30 +41,8 @@ int point_array_append( point_array_t* pa, point_t* p ) {
 int point_array_remove( point_array_t* pa, unsigned int i ) {
    if (pa != NULL && pa->points != NULL && i < pa->len && pa->len > 0) {
       pa->points[i] = pa->points[pa->len-1];
-      /*
-      pa->points[i].x = pa->points[pa->len-1].x;
-      pa->points[i].y = pa->points[pa->len-1].y;
-      pa->points[i].z = pa->points[pa->len-1].z;
-      */
       pa->len = pa->len-1;
       return 0;
    }
    return 1;
 }
-/*
-void point_array_init( point_array_t* pa );
-int point_array_append( point_array_t* pa, point_t* p );
-
-int main() {
-   point_array_t* pa;
-   //point_array_init(pa);
-   point_t* p;
-   p->x = 1;
-   p->y = 1;
-   p->z = 1;
-   printf("point p is: x:%g y:%g z:%g \n", p->x, p->y, p->z);
-   point_array_append(pa,p);
-   printf("point p is: x:%g y:%g z:%g \n", pa->points[0].x, pa->points[0].y, pa->points[0].z);
-   return 0;
-}
-*/
