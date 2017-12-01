e9b4f34a51df0a02b1bf5edad838475904fac322
jzl1 <jzl1@asb9838nu-d06.csil.sfu.ca>
2017-11-30 20:24:58 -0800

    wee

--- 8/t1.c
+++ 8/t1.c
@@ -28,9 +28,12 @@ int point_array_append( point_array_t* pa, point_t* p ) {
    if (pa != NULL) {
       pa->points = realloc(pa->points,sizeof(point_t)*(pa->len+1));
       if (pa->points != NULL) {
+         pa->points[pa->len] = p;
+         /*
          pa->points[pa->len].x = p->x;
          pa->points[pa->len].y = p->y;
          pa->points[pa->len].z = p->z;
+         */
          pa->len = pa->len+1;
          return 0;
       }
@@ -42,14 +45,14 @@ int point_array_append( point_array_t* pa, point_t* p ) {
 // the array by one. The order of points in the array may change.
 int point_array_remove( point_array_t* pa, unsigned int i ) {
    if (pa != NULL && pa->points != NULL && i < pa->len && pa->len > 0) {
+      pa->points[i] = pa->points[len-1];
+      /*
       pa->points[i].x = pa->points[pa->len-1].x;
       pa->points[i].y = pa->points[pa->len-1].y;
       pa->points[i].z = pa->points[pa->len-1].z;
+      */
       pa->len = pa->len-1;
-      pa->points = realloc(pa->points,sizeof(point_t)*(pa->len));
-      if (pa->points != NULL) {
-         return 0;
-      }
+      return 0;
    }
    return 1;
 }
