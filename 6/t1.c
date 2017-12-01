18ce1d6ea0bfd4951ae6a998ce495a7c56b3487a
jzl1 <jzl1@asb9838nu-d06.csil.sfu.ca>
2017-11-30 19:50:50 -0800

    twearfar

--- 6/t1.c
+++ 6/t1.c
@@ -50,13 +50,3 @@ intarr_t* intarr_load_binary( const char* filename ) {
    return NULL;
    }
 }
-
-int intarr_save_binary( intarr_t* ia, const char* filename );
-intarr_t* intarr_load_binary( const char* filename );
-
-int main() {
-   intarr_t* ia;
-   ia->len = 5
-   ia->data = malloc(sizeof(int)*ia->len) 
-   return 0
-}
