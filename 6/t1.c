a9621a80819ad2bd44af4ed411018169ff1a8655
jzl1 <jzl1@asb9838nu-d06.csil.sfu.ca>
2017-11-30 19:50:12 -0800

    twearfar

--- 6/t1.c
+++ 6/t1.c
@@ -40,14 +40,23 @@ intarr_t* intarr_load_binary( const char* filename ) {
             fread(&(ia->len),sizeof(int),1,f);
             ia->data = malloc(sizeof(int)*ia->len);
             if (ia->data != NULL) {
-                  fread(&(ia->data),sizeof(int),ia->len,f);
-                  fclose(f);
-                  return ia;
-               
+               fread(ia->data,sizeof(int),ia->len,f);
+               fclose(f);
+               return ia;
             }
          }
       fclose(f); 
       }
    return NULL;
    }
+}
+
+int intarr_save_binary( intarr_t* ia, const char* filename );
+intarr_t* intarr_load_binary( const char* filename );
+
+int main() {
+   intarr_t* ia;
+   ia->len = 5
+   ia->data = malloc(sizeof(int)*ia->len) 
+   return 0
 }
