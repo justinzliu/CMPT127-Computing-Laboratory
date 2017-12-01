bc23e53339bcc9bf3e7927eab54a3d9e6f19ac7a
jzl1 <jzl1@asb9838nu-d06.csil.sfu.ca>
2017-11-30 19:05:17 -0800

    weeee

--- 6/t1.c
+++ 6/t1.c
@@ -33,14 +33,14 @@ int intarr_save_binary( intarr_t* ia, const char* filename ) {
 intarr_t* intarr_load_binary( const char* filename ) {
    FILE* f = fopen(filename,"r");
    if (f != NULL) {
-      intarr_t* ia = malloc(sizeof(intarr_t));
-      if (ia != NULL) {
-         fread(&(ia->len),sizeof(unsigned int),1,f);
-         ia->data = malloc(sizeof(int)*ia->len);
-         if (ia->data != NULL) {
-            if (fread(&(ia->data),sizeof(int),ia->len,f) == ia->len) {
+      intarr_t* iaNew = malloc(sizeof(intarr_t));
+      if (iaNew != NULL) {
+         fread(&(iaNew->len),sizeof(unsigned int),1,f);
+         iaNew->data = malloc(sizeof(int)*iaNew->len);
+         if (iaNew->data != NULL) {
+            if (fread(&(iaNew->data),sizeof(int),iaNew->len,f) == iaNew->len) {
                fclose(f);
-               return ia;
+               return iaNew;
             }
          }
       } 
