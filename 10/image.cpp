17d05b4f3485fb8a1593a1f16e4343cb79e42807
Justin <jzl1@sfu.ca>
2017-12-03 10:11:48 -0800

    test

--- 10/image.cpp
+++ 10/image.cpp
@@ -24,8 +24,7 @@ int Image::resize( unsigned int width,  unsigned int height, uint8_t fillcolor )
    if (pixels != NULL) {
       delete [] pixels;
    }
-   //uint8_t* pixels = new uint8_t(width*height);
-   uint8_t pixels[width*height];
+   pixels = new uint8_t(width*height);
    if (pixels != NULL) {
       cols = width;
       rows = height;
