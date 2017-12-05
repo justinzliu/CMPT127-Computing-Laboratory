1648d9d0b70a7aa55ee898455c3f12209b5190f9
Justin <jzl1@sfu.ca>
2017-12-04 19:36:40 -0800

    test

--- 10/image2.cpp
+++ 10/image2.cpp
@@ -89,10 +89,9 @@ int Image::load( const char* filename ) {
          fread(&rows,sizeof(uint8_t*),1,f);
          unsigned int len = cols*rows;
          if (resize(cols,rows,0) == 0) {
-            for (int i=0; i<len; i++) {
-               if (fread(pixels,sizeof(uint8_t*),len,f) == len) {
-                  fclose(f);
-                  return 0;
+            if (fread(pixels,sizeof(uint8_t*),len,f) == len) {
+               fclose(f);
+               return 0;
                }
             }   
          }
