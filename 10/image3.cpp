6fe7931552ab125d11c4bab5359a3ee4ecc9f710
Justin <jzl1@sfu.ca>
2017-12-05 17:34:17 -0800

    test

---
+++ 10/image3.cpp
@@ -0,0 +1,142 @@
+#include <stdint.h> // for uint8_t
+
+class Image {
+  
+public:
+  unsigned int cols;
+  unsigned int rows;
+  uint8_t** pixels;
+  
+  /* Constructs an image of 0x0 pixels. */
+Image::Image() {
+   cols = 0;
+   rows = 0;
+   pixels = NULL;
+}
+  
+  /* Frees all memory allocated for img */
+Image::~Image() {
+   cols = 0;
+   rows = 0;
+   if (pixels != NULL) {
+      for (int i=0; i<rows; i++) {
+         delete [] pixels[i];
+      }
+      delete [] pixels;
+      pixels = NULL;
+   }
+}
+  
+  /* Changes the size of an image, allocating memory as necessary, and
+     setting all pixels to fillcolor. Returns 0 on success, or a
+     non-zero error code.*/ 
+  int resize( unsigned int width,  unsigned int height, uint8_t fillcolor ) {
+   //I would imagine this would be a good line of code... but apparently I'm getting a double free call.
+   /*
+   if (pixels != NULL) {
+      for (int i=0; i<height; i++) {
+         delete [] pixels[i];
+      }
+      delete [] pixels;
+      pixels = NULL;
+   }
+   */
+   pixels = new uint8_t[height];
+   if (pixels != NULL) {
+      for (int i=0; i<width; i++) {
+         pixels[i] = new uint8_t[width];
+         if pixels[i] == NULL {
+            return 1;
+         }
+      }
+      cols = width;
+      rows = height;
+      for (int i=0; i<height; i++) {
+         for (int j=0; j<width; i++) {
+            pixels[i][j] = fillcolor;
+         }
+      }
+      return 0;
+   }
+   return 1; 
+}
+  
+  /* Sets the color of the pixel at (x,y) to color. Returns 0 on
+     success, else a non-zero error code. If (x,y) is not a valid
+     pixel, the call fails and the image does not change.*/
+  int set_pixel( unsigned int x, unsigned int y, uint8_t color ) {
+   if (pixels != NULL && x < cols && y < rows) {
+      pixels[x][y] = color;
+      return 0;
+   }
+   return 1;
+}
+
+  
+  /* Gets the color of the pixel at (x,y) and stores at the address
+     pointed to by colorp. Returns 0 on success, else a non-zero error
+     code. */
+  int get_pixel( unsigned int x, unsigned int y, uint8_t* colorp ) {
+   if (pixels != NULL && colorp != NULL && x < cols && y < rows) {
+      *colorp = pixels[x][y];
+      return 0;
+   }
+   return 1;
+}
+  
+  /* Saves the image in the file filename. In a format that can be
+     loaded by load().  Returns 0 on success, else a non-zero error
+     code. */
+  int save( const char* filename ) {
+   if (filename != NULL) {
+      FILE* f = fopen(filename, "w");
+      if (f != NULL) {
+         unsigned int len = cols*rows;
+         if (len > 0) {
+            fwrite(&cols,sizeof(uint8_t*),1,f);
+            fwrite(&rows,sizeof(uint8_t*),1,f);
+            for (int i=0; i<rows; i++) {
+               if (fwrite(pixels[i],sizeof(uint8_t*),cols,f) != cols) {
+                  return 1;
+               }
+            }
+            fclose(f);
+            return 0;
+         }
+         else if (len == 0) {
+            fclose(f);
+            return 0;
+         }
+      fclose(f);
+      }
+   }
+   return 1;
+}
+
+  /* Load the an image from the file filename, replacing the current
+     image size and data. The file is in a format that was saved by
+     save().  Returns 0 success, else a non-zero error code . */
+  int load( const char* filename ) {
+   if (filename != NULL) {
+      FILE* f = fopen(filename,"r");
+      if (f != NULL) {
+         fread(&cols,sizeof(uint8_t*),1,f);
+         fread(&rows,sizeof(uint8_t*),1,f);
+         unsigned int len = cols*rows;
+         if (resize(cols,rows,0) == 0) {
+            for (int i=0; i<rows; i++) {
+               if (fread(pixels[i],sizeof(uint8_t*),cols,f) != cols) {
+                  return 1; 
+               }
+            }
+            fclose(f);
+            return 0;
+            }
+         }   
+      fclose(f);
+      }
+   }
+   //any errors or empty file
+   return 1;
+}
+
